6d3b76a2f1c6a1d74ed2d7dd5e988668a6168e0a
Doggy <68931098+DoggyOficial@users.noreply.github.com>
2024-02-05 09:51:18 -0300

    Delete netlify directory

--- netlify/upload.js
+++
@@ -1,9 +0,0 @@
-exports.handler = async function (event, context) {
-  // Lógica para lidar com o upload aqui
-  // Você pode usar bibliotecas como `multer` para lidar com o upload de arquivos
-
-  return {
-    statusCode: 200,
-    body: JSON.stringify({ message: 'Upload realizado com sucesso!' }),
-  };
-};
